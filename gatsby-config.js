094558aa0bc70aff8612f3a6bbb79ea0e84980d1
deuxpartout <151935168+deuxpartout@users.noreply.github.com>
2024-08-02 10:37:13 +0200

    Finished Gatsby Tutorial Part 4

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,11 +3,18 @@
  */
 module.exports = {
   siteMetadata: {
-    title: `My First Gatsby Site`,
+    title: `My Gatsby Site`,
     siteUrl: `https://main--gatsby-site-playground.netlify.app/`,
   },
   plugins: [
     "gatsby-plugin-image",
     "gatsby-plugin-sharp",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: `blog`,
+        path: `${__dirname}/blog`,
+      }
+    },
   ],
 }
