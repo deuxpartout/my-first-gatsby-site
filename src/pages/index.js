8e0b7a4e078cb5f6a8536c969febf712e80692e0
deuxpartout <151935168+deuxpartout@users.noreply.github.com>
2024-09-10 19:29:20 +0200

    Finished Gatsby Tutorial Part 3

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,7 +10,7 @@ const IndexPage = () => {
       <p>I'm making this by following the Gatsby Tutorial.</p>
       <StaticImage
         alt="Torre Guaceto"
-        src="../images/tg.jpg"
+        src="../images/tg.webp"
       />
     </Layout>
   )
