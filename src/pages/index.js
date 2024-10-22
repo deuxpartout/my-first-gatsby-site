557a8c19dae614336dd1e1c230914d193673d594
deuxpartout <nicola.quacquarelli@gmail.com>
2024-10-22 11:40:14 +0200

    Finished Gatsby Tutorial Part 3

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,15 @@
 import * as React from 'react'
 import Layout from '../components/layout'
+import { StaticImage } from 'gatsby-plugin-image'
 
 const IndexPage = () => {
   return (
       <Layout pageTitle="Home Page">
         <p>I'm making this by following the Gatsby Tutorial.</p>
+        <StaticImage
+        alt="La faggeta secolare della Foresta Umbra, Gargano"
+        src="../images/L1000288-2.jpg"
+      />
       </Layout>
   )
 }
